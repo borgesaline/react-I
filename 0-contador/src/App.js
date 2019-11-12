0d1d64417d943a04ffb3f620c62aaa7dcb08c53e
borgesaline <aline_borges@msn.com>
2019-11-12 15:32:39 -0300

    Exercicio 1 react

--- 0-contador/src/App.js
+++ 0-contador/src/App.js
@@ -17,13 +17,29 @@ class Contador extends React.Component {
     })
   }
 
+subtrairUm = () => {
+  this.setState(prevState => {
+    return {
+      contador: prevState.contador -1
+    }
+  })
+}
+
+resetar = () => {
+  this.setState (() => {
+  return{
+    contador: 0
+  }
+  })
+}
+
   render() {
     return (
       <div>
         <p>{this.state.contador}</p>
         <button className="btnMaisUm" onClick={this.adicionarUm}>+1</button>
-        <button className="btnMenosUm">-1</button>
-        <button className="btnResetar">resetar</button>
+        <button className="btnMenosUm" OnClick ={this.subtrairUm}>-1</button>
+        <button className="btnResetar" OnClick ={this}>resetar</button>
       </div>
     );
   }
